aab3d42fea7e7b3cd42816b65911fc8eeb05d3e0
willian-matheus <85637800+williamtesche@users.noreply.github.com>
2023-07-12 13:41:02 -0300

    Iniciando o projeto
    Fiz a instalação das bibliotecas.

---
+++ projeto/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TitleName from './Components/TiltleName.jsx';
+
+function App() {
+  return (
+    <div className="App">
+     <TitleName />
+    </div>
+  );
+}
+
+export default App;
---
+++ projeto/src/Components/TiltleName.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function TitleName() {
+    return (
+      <div id="Conteiner-Global">
+        <div id="Conteiner">
+         <h1>Bell T Boss</h1>
+        </div>
+      </div>
+    );
+  }
+  
+  export default TitleName;
